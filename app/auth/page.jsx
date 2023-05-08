0ffc8f1e389552e594bffe3899aacf169dcdac21
utsav82 <utsa.yadav.94@gmail.com>
2023-05-08 19:52:22 +0530

    Added basic components

---
+++ app/auth/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Auth() {
+  return (
+    <div
+        className="
+          justify-center 
+          items-center 
+          flex 
+          overflow-x-hidden 
+          overflow-y-auto 
+          fixed 
+          inset-0 
+          z-50 
+          outline-none 
+          focus:outline-none
+          bg-neutral-800/70
+        "
+      >
+      Auth page
+    </div>
+  )
+}
+
+export default Auth
---
+++ app/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+
+import Logo from "./Logo";
+const Navbar = () => {
+  return ( 
+    <div className="relative w-full bg-black z-10 shadow-sm">
+       <Logo></Logo>
+     </div>
+  );
+}
+
+
+export default Navbar;
